0e21b6e3e715be36d1dc9f832d343353d2f8ace7
Boolean <boolean405@gmail.com>
2025-06-03 02:10:34 +0700

    rateLimit temp change

--- server/middlewares/rateLimiter.js
+++ server/middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const rateLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000,
+  windowMs: 1 * 60 * 1000, 
   limit: 100,
   standardHeaders: true,
   legacyHeaders: false, 

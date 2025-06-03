b3bc68b365e4f9287aa9b997f9e5a7458d7209d7
Boolean <boolean405@gmail.com>
2025-06-03 22:35:12 +0700

    fix not found error

--- server/middlewares/notFoundHandler.js
+++ server/middlewares/notFoundHandler.js
@@ -1,8 +1,8 @@
 const notFoundHandler = (req, res, next) => {
   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
   res.status(404).json({
-    con: false,
-    msg: `Url not found - ${fullUrl}`,
+    status: false,
+    message: `Url not found - ${fullUrl}`,
   });
 };
 
